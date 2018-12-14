512646bdad94aa28a47ced1ad371ffc6f56a5750
william <willshinji@gmail.com>
2018-12-14 13:32:59 -0200

    set mongoose connection

---
+++ backend/src/database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGOLAB_URI || 'mongodb://localhost/todo'
+
+mongoose.connect(url, (error, response) => {
+    if (error) console.log (`ERROR connecting to:  ${url} -  ${error}`);
+    else console.log (`Succeeded connected to: ${url}`);
+})
+
+mongoose.Promise = global.Promise
+
+module.export = mongoose
