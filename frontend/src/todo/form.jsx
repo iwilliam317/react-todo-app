9b7d4f40baa59eddcf2b0265929224a48bb86716
william <willshinji@gmail.com>
2018-12-21 19:19:41 -0200

    create form

--- frontend/src/todo/form.jsx
+++ frontend/src/todo/form.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default props => (
-    <div>
-        <h2>Form</h2>
+    <div role='form' className='todoForm row'>
+        <div className='col-xs-12 col-sm-9 col-md-10'>
+            <input id='description' className='form-control' placeholder='Add a task'></input>
+        </div>
+        <div className='col-xs-12 col-sm-3 col-md-2'>
+            <button className='btn btn-primary'>
+                <i className='fa fa-plus'></i>
+            </button>
+        </div>
     </div>
 )
