61172c1e78a0cf661156d142ad70710b1bf2c267
william <willshinji@gmail.com>
2019-01-18 20:15:26 -0200

    set empty initial_state

--- frontend (redux)/src/todo/formReducer.jsx
+++ frontend (redux)/src/todo/formReducer.jsx
@@ -1,23 +1,4 @@
-const INITIAL_STATE = {
-    description: 'My description from redux',
-    list: [
-        {
-            _id: 1,
-            description: 'Dentist at 3 pm',
-            done: false
-        },
-        {
-            _id: 2,
-            description: 'Study redux',
-            done: false
-        },
-        {
-            _id: 3,
-            description: 'buy suplements',
-            done: true
-        }
-    ]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 const formReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
