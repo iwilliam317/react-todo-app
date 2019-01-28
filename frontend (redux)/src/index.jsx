5255e6f63b2fb3ef3edaedbebfc893924e6ba70d
william <willshinji@gmail.com>
2019-01-28 19:23:56 -0200

    set redux-thunk

--- frontend (redux)/src/index.jsx
+++ frontend (redux)/src/index.jsx
@@ -8,10 +8,11 @@ import { createStore, applyMiddleware } from 'redux';
 
 import promise from 'redux-promise'
 import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App />
