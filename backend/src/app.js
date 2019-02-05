26719c5a82f73679c4606173cf9bb293602bc961
william <willshinji@gmail.com>
2019-02-05 18:19:37 -0200

    deploy at heroku

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,5 +2,5 @@ const app = require('./config/server')
 
 const port = app.get('PORT')
 
-app.listen(port, () => console.log(`Backend is running on port ${port}`))
+app.listen(process.env.PORT || port, () => console.log(`Backend is running on port ${port}`))
 require('./config/routes')(app)
